d3a436eb4d85fa9125eb3c334e6128f9a3f1bda4
RichardCroxall <github@croxalls.co.uk>
2020-08-26 17:14:47 +0100

    Add sunset & sunrise calculation for anywhere in the world
    Add sunset & sunrise calculation for anywhere in the world.
    Initially Latitude and Longitude are hardcoded for Timsbury.
    Previously the values were copied from the Reeds Almanac from mid-90s for Poole, Dorset.
    Later the user will be able to set the Lat & Long in the rules.

---
+++ Compiler2/Calculations/SunSetRise.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Compiler2.Calculations
+{
+    public class SunSetRise
+    {
+        const int MiddayMinutes = 12 * 60;
+
+        public TimeSpan Sunset { get; private set; }
+        public TimeSpan Sunrise { get; private set; }
+
+        public SunSetRise(double latitudeDegrees, double longitudeDegrees, DateTime date)
+        {
+            Debug.Assert(latitudeDegrees <= 90);
+            Debug.Assert(latitudeDegrees >= -90);
+            Debug.Assert(longitudeDegrees <= 180);
+            Debug.Assert(longitudeDegrees >= -180);
+
+            DateTime endOfYear = new DateTime(date.Year, 12, 31);
+            int daysInYear = endOfYear.DayOfYear;
+            Debug.Assert(daysInYear >= 365 && daysInYear <= 366);
+
+
+            double fractionalYearGamma = 2 * Math.PI / daysInYear * (date.DayOfYear - 1 + (date.Hour - 12) / 24.0);
+
+            double equationOfTimeMinutes = 229.18 * (0.000075
+                                                     + 0.001868 * Math.Cos(fractionalYearGamma)
+                                                     - 0.032077 * Math.Sin(fractionalYearGamma)
+                                                     - 0.014615 * Math.Cos(2.0 * fractionalYearGamma)
+                                                     - 0.040849 * Math.Sin(2.0 * fractionalYearGamma));
+            double solarDeclinationRadians = 0.006918
+                                      - 0.399912 * Math.Cos(fractionalYearGamma)
+                                      + 0.070257 * Math.Sin(fractionalYearGamma)
+                                      - 0.006758 * Math.Cos(2.0 * fractionalYearGamma)
+                                      + 0.000907 * Math.Sin(2.0 * fractionalYearGamma)
+                                      - 0.002697 * Math.Cos(3.0 * fractionalYearGamma)
+                                      - 0.00148 * Math.Sin(3.0 * fractionalYearGamma);
+
+            double solarDeclinationDegrees = Degrees(solarDeclinationRadians);
+            const double maxSunDeclinationDegrees = 23.45;
+            Debug.Assert(solarDeclinationDegrees <= maxSunDeclinationDegrees);
+            Debug.Assert(solarDeclinationDegrees >= -maxSunDeclinationDegrees);
+
+            double sunriseZenithRadians = Radians(90.833);
+            double haRadians = Math.Acos(
+                Math.Cos(sunriseZenithRadians) / (Math.Cos(Radians(latitudeDegrees)) * Math.Cos(solarDeclinationRadians))
+                - Math.Tan(Radians(latitudeDegrees)) * Math.Tan(solarDeclinationRadians));
+
+            double sunriseMinutes = MiddayMinutes - 4 * (longitudeDegrees + Degrees(haRadians)) - equationOfTimeMinutes;
+            Sunrise = TimeSpan.FromMinutes(sunriseMinutes);
+            double sunsetMinutes = MiddayMinutes - 4 * (longitudeDegrees - Degrees(haRadians)) - equationOfTimeMinutes;
+            Sunset = TimeSpan.FromMinutes(sunsetMinutes);
+        }
+
+
+        private static double Radians(double degrees)
+        {
+            double radians = degrees * Math.PI / 180.0;
+            return radians;
+        }
+
+        private static double Degrees(double radians)
+        {
+            double degrees = radians * 180.0 / Math.PI;
+            return degrees;
+        }
+    }
+}
--- Compiler2/Code/CodeCalendar.cs
+++ Compiler2/Code/CodeCalendar.cs
@@ -24,7 +24,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
-
+using Compiler2.Calculations;
 using compiler2.Compile;
 using compiler2.Generate;
 
@@ -41,71 +41,11 @@ namespace compiler2.Code
         public static readonly TimeSpan SUNRISE_TIMESPAN = new TimeSpan(0, 0, -1);
         public static readonly TimeSpan SUNSET_TIMESPAN = new TimeSpan(0, 0, -2);
 
-        private const int JAN =       -1;
-        private const int FEB = (JAN + 31);
-        private const int MAR = (FEB + 29);
-        private const int APR = (MAR + 31);
-        private const int MAY = (APR + 30);
-        private const int JUN = (MAY + 31);
-        private const int JUL = (JUN + 30);
-        private const int AUG = (JUL + 31);
-        private const int SEP = (AUG + 31);
-        private const int OCT = (SEP + 30);
-        private const int NOV = (OCT + 31);
-        private const int DEC = (NOV + 30);
-
-        // define static method to make it easier to define dates and times of
-        // sunrise and sunset at Poole Harbour.
-        private static int TIME(int hour, int minute)
-        {
-            Debug.Assert(hour >= 0 && hour <= 23);
-            Debug.Assert(minute >= 0 && minute <= 59);
-            return ((hour) * 3600 + (minute) * 60);
-        }
-
-        struct astronomy_t
-        {
-	        public int	iDayOfYear;
-            public int /*time_t*/	tSunRise;
-            public int /*time_t*/	tSunSet;
-
-            public astronomy_t(int dayOfYear, int sunrise, int sunset)
-            {
-                iDayOfYear = dayOfYear;
-                tSunRise = sunrise;
-                tSunSet = sunset;
-            }
-        };
+        double latitude = 51.019570;//+ve is °N - Timsbury
+        double longitude = -1.505470;//+ve is °E - Timsbury
 
 
-        static astronomy_t[] pooleDayLight = new astronomy_t[]
-        {
-	        new astronomy_t(JAN +  1, TIME( 8, 10), TIME(16, 13)),
-	        new astronomy_t(JAN + 15, TIME( 8,  4), TIME(16, 31)),
-	        new astronomy_t(FEB +  1, TIME( 7, 44), TIME(16, 59)),
-	        new astronomy_t(FEB + 15, TIME( 7, 21), TIME(17, 24)),
-	        new astronomy_t(MAR +  1, TIME( 6, 52), TIME(17, 49)),
-	        new astronomy_t(MAR + 15, TIME( 6, 22), TIME(18, 12)),
-	        new astronomy_t(APR +  1, TIME( 5, 44), TIME(18, 40)),
-	        new astronomy_t(APR + 15, TIME( 5, 14), TIME(19,  3)),
-	        new astronomy_t(MAY +  1, TIME( 4, 43), TIME(19, 28)),
-	        new astronomy_t(MAY + 15, TIME( 4, 20), TIME(19, 50)),
-	        new astronomy_t(JUN +  1, TIME( 4,  1), TIME(20, 11)),
-	        new astronomy_t(JUN + 15, TIME( 3, 54), TIME(20, 22)),
-	        new astronomy_t(JUL +  1, TIME( 3, 59), TIME(20, 24)),
-	        new astronomy_t(JUL + 15, TIME( 4, 12), TIME(20, 15)),
-	        new astronomy_t(AUG +  1, TIME( 4, 34), TIME(19, 53)),
-	        new astronomy_t(AUG + 15, TIME( 4, 55), TIME(19, 28)),
-	        new astronomy_t(SEP +  1, TIME( 5, 21), TIME(18, 53)),
-	        new astronomy_t(SEP + 15, TIME( 5, 43), TIME(18, 22)),
-	        new astronomy_t(OCT +  1, TIME( 6,  8), TIME(17, 46)),
-	        new astronomy_t(OCT + 15, TIME( 6, 30), TIME(17, 46)),
-	        new astronomy_t(NOV +  1, TIME( 6, 59), TIME(17, 16)),
-	        new astronomy_t(NOV + 15, TIME( 7, 23), TIME(16, 43)),
-	        new astronomy_t(DEC +  1, TIME( 7, 47), TIME(16, 22)),
-	        new astronomy_t(DEC + 15, TIME( 8,  3), TIME(16,  3)),
-	        new astronomy_t(DEC + 31, TIME( 8, 10), TIME(16, 13))
-        };
+
         static private readonly dayEnum[] dayOfWeekMap =
         {
             dayEnum.DAY_SUN,
@@ -127,37 +67,6 @@ namespace compiler2.Code
             public dayEnum dayEnum;
             public int sunRise;
             public int sunSet;
-        }
-
-        static int /*time_t*/ getSunRise(int yearDayNo)
-        {
-	        int i = 1;
-	        Debug.Assert(yearDayNo >= 0 && yearDayNo <= 365);
-
-            while (i < pooleDayLight.Length && yearDayNo > pooleDayLight[i].iDayOfYear)
-	        {
-		        i++;
-	        }
-
-	        return pooleDayLight[i - 1].tSunRise +
-		        (yearDayNo -  pooleDayLight[i - 1].iDayOfYear) *
-		        (pooleDayLight[i].tSunRise - pooleDayLight[i - 1].tSunRise)/
-		        (pooleDayLight[i].iDayOfYear - pooleDayLight[i - 1].iDayOfYear);
-        }
-
-        static int /*time_t*/ getSunSet(int yearDayNo)
-        {
-	        int i = 1;
-	        Debug.Assert(yearDayNo >= 0 && yearDayNo <= 365);
-
-	        while (i <pooleDayLight.Length && yearDayNo > pooleDayLight[i].iDayOfYear)
-	        {
-		        i++;
-	        }
-	        return pooleDayLight[i - 1].tSunSet +
-		        ((yearDayNo -  pooleDayLight[i - 1].iDayOfYear) *
-		        (pooleDayLight[i].tSunSet - pooleDayLight[i - 1].tSunSet))/
-		        (pooleDayLight[i].iDayOfYear - pooleDayLight[i - 1].iDayOfYear);
         }
 
         public static int NoCalendarEntries
@@ -189,9 +98,10 @@ namespace compiler2.Code
 		        Debug.Assert(dateTime.Second == 0);  //really is midnight!
 		        Debug.Assert(dateTime.Minute == 0);
 		        Debug.Assert(dateTime.Hour == 0);
+                SunSetRise sunSetRise = new SunSetRise(latitude, longitude, dateTime);
 
-		        mtSunRise[iDayNo] = getSunRise(dateTime.DayOfYear - 1);
-		        mtSunSet[iDayNo] = getSunSet(dateTime.DayOfYear - 1);
+                mtSunRise[iDayNo] = (int) Math.Round(sunSetRise.Sunrise.TotalMilliseconds /1000.0);
+		        mtSunSet[iDayNo] = (int) Math.Round(sunSetRise.Sunset.TotalMilliseconds/1000.0);
 
 		        // set day of week bit
                 muDayAttributes[iDayNo] = dayOfWeekMap[(int) dateTime.DayOfWeek];
